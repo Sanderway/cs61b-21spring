28ef22387ac26371e6f024a1cb980d0091bdf0fe
Sanderway <sihangbin@gmail.com>
2023-11-12 23:47:39 +0800

    done with lab2 debugging

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,11 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+    @Test
+    public void testSquarePrimesSimple2() {
+        IntList lst = IntList.of(1,2,5,9,24,63,67);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 4 -> 25 -> 9 -> 24 -> 63 -> 4489", lst.toString());
+        assertTrue(changed);
+    }
 }
